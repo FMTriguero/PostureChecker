9ae6e9417e2b5a9332e95d2fe17bec6fde053b11
FMTriguero <FMTriguero@gmail.com>
2021-05-19 22:12:17 +0800

    tools, processing data through Keypoint_finder

--- tools.py
+++ tools.py
@@ -4,17 +4,32 @@ import time
 import os
 
 
+def get_all_directories(base_path="Saving_screenshots"):
+    directories = [f for f in os.listdir(base_path) if
+                   os.path.isdir(os.path.join(base_path, f))]
+    return directories
+
+
+def get_all_files_in_directory(directories, base_path="Saving_screenshots"):
+    files = []
+    for directory in directories:
+        files = files + [f for f in os.listdir(os.path.join(base_path, directory)) if
+                         os.path.isfile(os.path.join(base_path, directory, f))]
+    return files
+
+
 class Tools:
 
     def __init__(self, classifier=None, camera=None):
         self.classifier = classifier
         self.camera = camera
+        self.base_path = "Saving_screenshots"
 
     def take_screenshots_for_posture_data(self, posture, number_pictures, wait_time):
         print("Please keep a " + posture + " while this tests runs")
         print("Initiating\n")
         time.sleep(5)
-        folder_path = os.path.join("Saving_screenshots", posture)
+        folder_path = os.path.join(self.base_path, posture)
         os.mkdir(folder_path)
         for i in range(number_pictures):
             time.sleep(wait_time)
@@ -23,23 +38,30 @@ class Tools:
                                         path=(os.path.join(folder_path, posture + "_" + str(i) + ".jpg")))
             print("Taken " + str(i) + " images")
 
-    def key_point_extraction_data(self):
-        self.predict_image_data()
-        self.classifier.keypoints_finder.test_show_image()
+    def transform_screenshots_to_key_point_data(self):
+        directories = get_all_directories()
+        files = get_all_files_in_directory(directories)
+        self.key_point_extraction_data(directories, files)
 
-    def predict_image_data(self):
-        self.classifier.predict(image_path=os.path.join("Saving_screenshots", "Good_Posture_Playtime", "Good_Posture_Playtime_0.jpg"))
+    def key_point_extraction_data(self, directories, files):
+        for directory in directories:
+            new_directory = directory + "_key_points"
+            os.mkdir(os.path.join(self.base_path, new_directory))
+            for file in files:
+                self.classifier.predict(os.path.join(self.base_path, directory, file))
+                self.classifier.keypoints_finder.draw_peaks_over_canvas(black_image=True)
+                self.camera.save_screenshot(image=self.classifier.keypoints_finder.canvas,
+                                            path=os.path.join(self.base_path, new_directory, file))
 
 
 def running_tools():
     classifier = Classifier()
     camera = Camera()
 
-    # tools = Tools(camera=camera)
     tools = Tools(classifier=classifier, camera=camera)
 
     # tools.take_screenshots_for_posture_data("Bad_Posture_SquishNeck", 10, 3)
-    tools.key_point_extraction_data()
+    tools.transform_screenshots_to_key_point_data()
 
 
 running_tools()
